e2a926167aa39cb11bce949b86d3af8b0b1ee19b
IIDelta <jaustind1@gmail.com>
2024-07-16 12:58:48 -0400

    attempt for a complex project list page

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,8 +1,12 @@
 # app/urls.py
 
 from django.urls import path
-from .views import ClientListView  # Import the view
+from .views import ProjectListView, ProjectDetailView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView
 
 urlpatterns = [
-    path('clients/', ClientListView.as_view(), name='client_list'),  # Map the URL to the view
+    path('projects/', ProjectListView.as_view(), name='project-list'),
+    path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+    path('projects/new/', ProjectCreateView.as_view(), name='project-create'),
+    path('projects/<int:pk>/edit/', ProjectUpdateView.as_view(), name='project-edit'),
+    path('projects/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project-delete'),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,9 +1,81 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from django.views import View
-from django.shortcuts import render
-from app.models import Client
+from .models import (
+    Project,
+    ProjectStatus,
+    ProjectResponsibleParty,
+    ProjectDeliverables,
+    ProjectTherapeuticArea,
+    ProjectIngredientCategory,
+    ProjectIngredients,
+    Client,
+)
 
 
 class ClientListView(View):
     def get(self, request):
         clients = Client.objects.all()
         return render(request, "client_list.html", {"clients": clients})
+
+
+class ProjectListView(View):
+    def get(self, request):
+        projects = Project.objects.all()
+        project_id = request.GET.get("project_id")
+        project_name = request.GET.get("project_name")
+        sponsor_name = request.GET.get("sponsor_name")
+
+        if project_id:
+            projects = projects.filter(projectid__icontains=project_id)
+        if project_name:
+            projects = projects.filter(projectname__icontains=project_name)
+        if sponsor_name:
+            projects = projects.filter(sponsorname__icontains=sponsor_name)
+
+        return render(request, "project_list.html", {"projects": projects})
+
+
+class ProjectDetailView(View):
+    def get(self, request, pk):
+        project = get_object_or_404(Project, pk=pk)
+        project_status = ProjectStatus.objects.filter(projectid=project)
+        responsible_party = ProjectResponsibleParty.objects.filter(projectid=project)
+        deliverables = ProjectDeliverables.objects.filter(projectid=project)
+        therapeutic_areas = ProjectTherapeuticArea.objects.filter(projectid=project)
+        ingredient_categories = ProjectIngredientCategory.objects.filter(
+            projectid=project
+        )
+        ingredients = ProjectIngredients.objects.filter(projectid=project)
+
+        context = {
+            "project": project,
+            "project_status": project_status,
+            "responsible_party": responsible_party,
+            "deliverables": deliverables,
+            "therapeutic_areas": therapeutic_areas,
+            "ingredient_categories": ingredient_categories,
+            "ingredients": ingredients,
+        }
+        return render(request, "project_detail.html", context)
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    fields = ["projectid", "projectname", "sponsorname"]
+    template_name = "project_form.html"
+    success_url = reverse_lazy("project-list")
+
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    fields = ["projectid", "projectname", "sponsorname"]
+    template_name = "project_form.html"
+    success_url = reverse_lazy("project-list")
+
+
+class ProjectDeleteView(DeleteView):
+    model = Project
+    template_name = "project_confirm_delete.html"
+    success_url = reverse_lazy("project-list")
